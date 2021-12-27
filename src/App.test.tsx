f0a591792ad1b6c83f1aace8ef095d1902cb2a5b
shaki <shakiralihamza05@gmail.com>
2021-12-27 23:58:11 +0500

    ...

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {QUERY_LAUNCH_LIST} from "./App";
+// import App from "./App";
+// import TestRenderer from 'react-test-renderer';
+// import { MockedProvider } from '@apollo/client/testing';
+// const mocks:any = []; // We'll fill this in next
+
+test('renders App component', () => {
+    /*  const component = TestRenderer.create(
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <App/>
+          </MockedProvider>,
+      );*/
+    expect(0).toBe(0)
+});
